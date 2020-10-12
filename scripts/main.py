6e6164dc339d7c0331bb16a19a0596557de58630
Juan Carlo Rebanal <jcrebanal17@gmail.com>
2020-10-12 01:02:22 -0700

    implemented variable scroll speed and fixed bugs, implemented arrows rotated about their center

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,7 @@ from textprint import TextPrint
 from init_and_fps import *
 import overlay
 from math import floor 
+import os
 
 """
 Constants
@@ -17,9 +18,10 @@ left_offset = 10
 top_offset = 10
 
 rect_width = (surf_x_size - left_offset) // num_inputs
-rect_height = 20
+rect_height = 10 # default height in case of tap note
 
-colors = [(128, 128, 128), (255, 0, 0), (255, 165, 0), (255, 255, 0), (128, 255, 0), (0, 128, 0), (0, 0, 255), (75, 0, 130), (238, 130, 238)]
+#colors = [(128, 128, 128), (255, 0, 0), (255, 165, 0), (255, 255, 0), (128, 255, 0), (0, 128, 0), (0, 0, 255), (75, 0, 130), (238, 130, 238)]
+colors = [(128, 128, 128), (255, 255, 0), (255, 128, 0), (255, 0, 0), (255, 51, 255), (153, 51, 255), (51, 51, 255), (255, 153, 153), (153, 153, 255)]
 lefts = [x for x in range(left_offset, surf_x_size-rect_width, rect_width)]
 tops = [top_offset]*num_inputs
 notes.initNotes(colors, lefts, tops, rect_width, rect_height)
@@ -27,6 +29,8 @@ curr_colors = []
 curr_lefts = []
 curr_tops = [] 
 curr_rects = []
+arrow_start_x = left_offset 
+arrow_start_y = top_offset
 
 xb_button_map = {0: "A", 1: "B", 2: "X", 3: "Y", 4: "LB", 5: "RB", 6: "Back", 7: "Start", 8: "L3", 9: "R3"}
 xb_valid_button_nums = set([0, 1, 2, 3, 4, 5])
@@ -52,12 +56,11 @@ screen, clock = init_screen_and_clock(surf_x_size, surf_y_size, app_name)
 fonts = create_fonts([32, 16, 14, 8])
 fps = 60
 
-i = 1
-r = notes.getRect(i, i, 20, 20)
+
 
 dt = clock.tick(fps)
 vx = 0.0
-vy = 0.08
+vy = 0.6 # at vy=0.1, windows when creating nodes = windows when playing notes with no rect height scaling factor
 run = True 
 
 # Joystick
@@ -69,7 +72,6 @@ textPrint = TextPrint(x_pos=10, y_pos=50)
 buttonPressed = False 
 buttonReleased = False
 
-prevDir = 5 # last direction the stick was pushed
 
 debug = False
 
@@ -104,7 +106,31 @@ for key, val in overlay_map.items():
 overlay_x = 300
 overlay_y = 50
 
+# Stepmania arrows for direction
+img_dir = os.path.join(os.getcwd(), "resources", "images")
+arrow_scale_factor = 0.5 # vy 0.4 and scale 0.75 and vertical offset 32
+arrow_vertical_spawn_offset = 64
+arrow_dir = os.path.join(img_dir, "USWdefault9SM5")
 
+arrows = {1: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_downback.png")), 0.0, arrow_scale_factor),
+          2: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_down.png")), 0.0, arrow_scale_factor),
+          3: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_downfwd.png")), 0.0, arrow_scale_factor),
+          4: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_back.png")), 0.0, arrow_scale_factor),
+          6: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_fwd.png")), 0.0, arrow_scale_factor),
+          7: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_upback.png")), 0.0, arrow_scale_factor),
+          8: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_up.png")), 0.0, arrow_scale_factor),
+          9: pygame.transform.rotozoom(pygame.image.load(os.path.join(arrow_dir, "arrow_upfwd.png")), 0.0, arrow_scale_factor)
+}
+
+
+
+
+
+prevDir = 5 # direction stick pointed last in numpad notation
+
+tap_window = 8 # buttons held for longer than this many frames become holds
+
+mode = "rec" # or "play"
 
 while run:
     rectsLeavingThisFrame = 0
@@ -225,7 +251,10 @@ while run:
             xb_held_buttons[xb_button_map[button_num]] += 1
             print(xb_held_buttons[xb_button_map[button_num]], xb_button_map[button_num])
         if not button and xb_held_buttons_frames[xb_button_map[button_num]]:
-            notes.addNote(xb_button_map[button_num], curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=xb_held_buttons_frames[xb_button_map[button_num]])
+            if xb_held_buttons_frames[xb_button_map[button_num]] > tap_window:
+                notes.addNote(xb_button_map[button_num], curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=xb_held_buttons_frames[xb_button_map[button_num]] * (vy/0.1))
+            else:
+                notes.addNote(xb_button_map[button_num], curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=rect_height)
             xb_held_buttons_frames[xb_button_map[button_num]] = 0
 
 
@@ -242,39 +271,50 @@ while run:
         xb_held_buttons[xb_axis_map[axis]] += 1
         print(xb_held_buttons[xb_axis_map[axis]], xb_axis_map[axis])        
     elif axis in (0.0, -0.0) and xb_axis_released:
-        notes.addNote(xb_axis_released, curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=xb_held_buttons_frames[xb_axis_released])
+        if xb_held_buttons_frames[xb_axis_released] > tap_window:
+            notes.addNote(xb_axis_released, curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=xb_held_buttons_frames[xb_axis_released] * (vy/0.1))
+        else:
+            notes.addNote(xb_axis_released, curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=rect_height)
         xb_axis_released = ""
 
     hat = joystick.get_hat(0)
     textPrint.print(screen, "{}".format(xb_dir_map[hat]))
+    # TODO fix for hold directions ; charge moves
     if xb_dir_map[hat] != 5 and xb_dir_map[hat] != prevDir:
-        notes.addNote("Dir", curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=2)
+        notes.addNote("Dir", curr_colors, curr_lefts, curr_tops, curr_rects, rect_w=rect_width, rect_h=rect_height, direction=xb_dir_map[hat])
     screen.blit(overlay_balltop, [overlay_x+overlay_map[hat][0], overlay_y+overlay_map[hat][1]])
     prevDir = xb_dir_map[hat]
-
 
      
     """ End Xbox 360 """
 
-    # Hello world/animation test rectangle, for debugging
-    r = r.move(vx*dt, vy*dt)
 
     for j in range(len(curr_rects)):
-        curr_rects[j] = curr_rects[j].move(vx*dt, vy*dt)
-        pygame.draw.rect(screen, curr_colors[j], curr_rects[j])
-        if curr_rects[j].bottom > surf_y_size:
-            rectsLeavingThisFrame += 1
+        if isinstance(curr_rects[j], pygame.Rect):
+            curr_rects[j].left += int(vx*dt)
+            curr_rects[j].top += int(vy*dt)
+            pygame.draw.rect(screen, curr_colors[j], curr_rects[j])
+            if curr_rects[j].top > surf_y_size:
+                rectsLeavingThisFrame += 1
+
+        else: # is image
+            curr_colors[j][0] += int(vx*dt)
+            curr_colors[j][1] += int(vy*dt)
+            screen.blit(arrows[curr_rects[j]], [curr_colors[j][0], curr_colors[j][1] - arrow_vertical_spawn_offset])
+            if curr_colors[j][1] > surf_y_size:
+                rectsLeavingThisFrame += 1
+
+        
 
     notes.clearNote(rectsLeavingThisFrame, curr_colors, curr_lefts, curr_tops, curr_rects)
 
     textPrint.print(screen, "Rects in memory: {}".format(str(len(curr_rects))))
 
     display_fps(fonts)
-    pygame.draw.rect(screen, (255,0,255,255), r)
 
     # draw judgment line , TODO put into library later
     judgmentLineDistFromBottom = 100
-    pygame.draw.line(screen, (255, 255, 255), (0, surf_y_size-judgmentLineDistFromBottom), (surf_x_size, surf_y_size-judgmentLineDistFromBottom))
+    pygame.draw.line(screen, (255, 255, 255), (0, surf_y_size-judgmentLineDistFromBottom), (surf_x_size, surf_y_size-judgmentLineDistFromBottom), 3)
 
     pygame.display.update()
     clock.tick(fps)
--- scripts/notes.py
+++ scripts/notes.py
@@ -14,13 +14,21 @@ def initNotes(colors_, lefts_, tops_, rect_width_, rect_height_):
     rect_width = rect_width_
     rect_height = rect_height_
 
-def addNote(input_type, curr_colors, curr_lefts, curr_tops, curr_rects, rect_w, rect_h):
-    print('Called addNote: ', input_type)
-    inputs = {"Dir": 0, "X": 1, "A": 2, "Y": 3, "B": 4, "RB": 5, "RT": 6, "LB": 7, "LT": 8}
-    curr_colors.append(colors[inputs[input_type]])
-    curr_lefts.append(lefts[inputs[input_type]])
-    curr_tops.append(tops[inputs[input_type]])
-    curr_rects.append(getRect(curr_lefts[-1], curr_tops[-1], rect_w, rect_h))
+def addNote(input_type, curr_colors, curr_lefts, curr_tops, curr_rects, rect_w, rect_h, direction=None):
+    print('Called addNote: ', input_type) # debug
+    #inputs = {"Dir": 0, "X": 1, "A": 2, "Y": 3, "B": 4, "RB": 5, "RT": 6, "LB": 7, "LT": 8}
+    inputs = {"Dir": 0, "X": 1, "Y": 2, "RB": 3, "A": 4, "B": 5, "RT": 6, "LB": 7, "LT": 8}
+    directions = {(0, 0): 5, (1, 0): 6, (0, -1): 2, (-1, 0): 4, (0, 1): 8, (1, -1): 3, (-1, -1): 1, (-1, 1): 7, (1, 1): 9}
+    if input_type != "Dir":
+        curr_colors.append(colors[inputs[input_type]])
+        curr_lefts.append(lefts[inputs[input_type]])
+        curr_tops.append(tops[inputs[input_type]])
+        curr_rects.append(getRect(curr_lefts[-1], curr_tops[-1], rect_w, rect_h))
+    else: # 
+        curr_colors.append([lefts[inputs[input_type]], tops[inputs[input_type]]])
+        curr_lefts.append(0)
+        curr_tops.append(0)
+        curr_rects.append(direction)
 
 def clearNote(num_to_pop, curr_colors, curr_lefts, curr_tops, curr_rects):
     for i in range(num_to_pop):
